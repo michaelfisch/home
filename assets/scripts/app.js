817a7ac53540bd1fa543b9cac7aa8b52ea422d17
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2019-02-26 09:35:34 -0500

    Updated text from id writing text

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -72,7 +72,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also love the Boston Red Sox.", "solve problems."
+				"am a Digital Marketing Analyst.", "am a Customer Success Specialist", "and a Frontend Web Developer", "am passionate about cooking", "also love the Boston Red Sox.", "am a team player", "work hard", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
