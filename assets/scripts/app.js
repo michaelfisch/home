e1f4f9a6961b7b2c88f79eb4734b5d241a19cc1e
GiantSlayer1913 <michaeljaredfisch@gmail.com>
2019-02-26 15:35:07 -0500

    Updated h2 from index and function makeWords from app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -72,7 +72,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Digital Marketing Analyst, Customer Success Specialist and a Frontend Web Developer", "am passionate about cooking", "also love the Boston Red Sox.", "am a team player", "work hard", "solve problems."
+				"I'm a Customer Success Specialist and a Frontend Web Developer", "I'm passionate about frontend development, sports and cooking.", "I'm a team player and work hard.", "But more importantly...", "I solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
